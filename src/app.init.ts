88c2470209d0d8b4ba27b4ee66b16f11bfe1540b
riccardotornesello <riccardo.tornesello@gmail.com>
2022-12-26 15:54:08 +0100

    split app initialization in other file

---
+++ src/app.init.ts
@@ -0,0 +1,56 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  UnprocessableEntityException,
+  ValidationError,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { HttpAdapterHost, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+import {
+  AllExceptionsFilter,
+  HttpExceptionFilter,
+  UnprocessableEntityExceptionFilter,
+} from './filters';
+
+export function initApp(app: INestApplication) {
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) =>
+        new UnprocessableEntityException(errors),
+    }),
+  );
+
+  app.useGlobalFilters(
+    new AllExceptionsFilter(httpAdapter),
+    new HttpExceptionFilter(httpAdapter),
+    new UnprocessableEntityExceptionFilter(httpAdapter),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS project')
+    .setDescription('A simple NestJS boilerplate')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,12 @@
-import {
-  ClassSerializerInterceptor,
-  UnprocessableEntityException,
-  ValidationError,
-  ValidationPipe,
-  VersioningType,
-} from '@nestjs/common';
-import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { useContainer } from 'class-validator';
-import helmet from 'helmet';
+import { NestFactory } from '@nestjs/core';
 
+import { initApp } from './app.init';
 import { AppModule } from './app.module';
-import {
-  AllExceptionsFilter,
-  HttpExceptionFilter,
-  UnprocessableEntityExceptionFilter,
-} from './filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
-
-  const httpAdapter = app.get(HttpAdapterHost);
-
-  app.use(helmet());
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      exceptionFactory: (errors: ValidationError[]) =>
-        new UnprocessableEntityException(errors),
-    }),
-  );
-
-  app.useGlobalFilters(
-    new AllExceptionsFilter(httpAdapter),
-    new HttpExceptionFilter(httpAdapter),
-    new UnprocessableEntityExceptionFilter(httpAdapter),
-  );
-
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('NestJS project')
-    .setDescription('A simple NestJS boilerplate')
-    .setVersion('1.0')
-    .addTag('nestjs')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('swagger', app, document);
+  initApp(app);
 
   await app.listen(3000);
 }

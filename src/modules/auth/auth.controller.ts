fd975320bbe74150343576090bf4841f05da9b54
riccardotornesello <riccardo.tornesello@gmail.com>
2022-12-26 15:13:33 +0100

    remove duplicate check

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -30,40 +30,6 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() userRegisterDto: UserRegisterDto) {
-    await Promise.all([
-      this.userService.getByUsername(userRegisterDto.username),
-      this.userService.getByEmail(userRegisterDto.email),
-    ]).then((users) => {
-      const [userByUsername, userByEmail] = users;
-      const errors: ValidationError[] = [];
-
-      if (userByUsername) {
-        errors.push({
-          target: userRegisterDto,
-          property: 'username',
-          value: userRegisterDto.username,
-          constraints: {
-            isUnique: 'Username already exists',
-          },
-        });
-      }
-
-      if (userByEmail) {
-        errors.push({
-          target: userRegisterDto,
-          property: 'email',
-          value: userRegisterDto.email,
-          constraints: {
-            isUnique: 'Email already exists',
-          },
-        });
-      }
-
-      if (errors.length > 0) {
-        throw new UnprocessableEntityException(errors);
-      }
-    });
-
     const user = await this.authService.registerUser(userRegisterDto);
     const token = await this.authService.generateAuthToken(user);
 

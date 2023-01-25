1cdefff4018ac5a79ad2fad0bb12101fd3e7a825
riccardotornesello <riccardo.tornesello@gmail.com>
2023-01-25 14:44:45 +0400

    improve module imports

--- src/modules/app/auth/auth.module.ts
+++ src/modules/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { forwardRef, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
@@ -7,10 +7,11 @@ import { AuthService } from './auth.service';
 import { AuthToken } from './entities/auth-token.entity';
 import { EmailVerification } from './entities/email-verification.entity';
 
+@Global()
 @Module({
   imports: [
     MikroOrmModule.forFeature([AuthToken, EmailVerification]),
-    forwardRef(() => UserModule),
+    UserModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- src/modules/app/user/user.module.ts
+++ src/modules/app/user/user.module.ts
@@ -1,13 +1,12 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { AuthModule } from '../auth/auth.module';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
+  imports: [MikroOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
